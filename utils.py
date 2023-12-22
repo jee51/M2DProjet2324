dccf3083997c1aa1f376bfb0da7977ee79c95e2b
DINH Quang Dung (Đinh Quang Dũng) <dqdung999@gmail.com>
2023-12-22 10:55:30 +0100

    Add files via upload
    Some utility functions for data analysis

---
+++ utils.py
@@ -0,0 +1,99 @@
+import numpy as np
+import pandas as pd
+
+def detect_phase(df, altitude_threshold = 0.95):
+    '''
+        df['M [Mach]'] == 0 => taxi
+        df['ALT [ft]] == 0.95*max altitude => cruise because using alt is more stable
+        other = climb/descend
+    '''
+    #Get taxi idx
+    #There are 2 taxi phases
+    mc = df['M [Mach]']
+    idx = mc [ mc == 0 ].index
+    idx1 = idx[idx < len(mc)//2]
+    idx2 = idx[idx > len(mc)//2]
+
+    if len(idx1) == 0 or len(idx2) == 0:
+        print('Record is incomplete')
+        return False
+    else:
+        taxi1_idx = min(idx1), max(idx1)
+        taxi2_idx = min(idx2), max(idx2)
+
+    #Get cruise idx
+    a = df['ALT [ft]']
+    idx = a [ ( a > max(a)*(altitude_threshold) ) ].index
+    cruise_idx = min(idx), max(idx)
+
+    #Get climb idx
+    climb_idx = taxi1_idx[1], cruise_idx[0]
+    
+    #Get descend idx
+    descend_idx = cruise_idx[1], taxi2_idx[0]
+
+    return taxi1_idx, climb_idx, cruise_idx, descend_idx, taxi2_idx 
+
+def get_consumption(ac,phase = None):
+    '''
+        Input: 
+            ac: .h5 files of flight recordings
+            phase: None (the whole flight) or one of the four 'taxi','climb','cruise','descend'
+        Output:
+            Fuel consumption of specified phase
+    '''
+
+    dat = []
+    for i, df in enumerate(ac):
+
+        if len(df.columns)>0 and "ALT [ft]" in df.columns:
+            
+            alt = df["ALT [ft]"]
+            Alt_max = max(alt)
+
+            if Alt_max > 20000:
+                
+                ## selecting phase index
+                phases = detect_phase(df,0.95) # phases = taxi1_idx, climb_idx, cruise_idx, descend_idx, taxi2_idx
+
+                if phases == False:
+                    print(i)
+                    continue
+                else:
+                    taxi1_idx, climb_idx, cruise_idx, descend_idx, taxi2_idx = phases
+
+                if phase == 'taxi':
+                    idx = np.concatenate([np.arange(taxi1_idx[0],taxi1_idx[1]),
+                                          np.arange(taxi2_idx[0],taxi2_idx[1])])
+                elif phase == 'taxi1':
+                    idx = np.arange(taxi1_idx[0],taxi1_idx[1])
+                elif phase == 'taxi2':
+                    idx = np.arange(taxi2_idx[0],taxi2_idx[1])
+                elif phase == 'climb':
+                    idx = np.arange(climb_idx[0],climb_idx[1])
+                elif phase == 'cruise':
+                    idx = np.arange(cruise_idx[0],cruise_idx[1])
+                elif phase == 'descend':
+                    idx = np.arange(descend_idx[0],descend_idx[1])    
+                else:
+                    idx = np.arange(0,len(df))  
+                            
+                df_phase = df.iloc[idx] 
+
+                consumption_engine1 = df_phase['Q_1 [lb/h]'].sum()/(3600*2.205) #(to kg)
+                consumption_engine2 = df_phase['Q_2 [lb/h]'].sum()/(3600*2.205) #(to kg)
+                total_consumption = consumption_engine1 + consumption_engine2
+
+                C1 = consumption_engine1/0.73 
+                C2 = consumption_engine2/0.73 
+
+                phase_duration = len(df_phase)/3600 # in hour        
+                Mach_max = df["M [Mach]"].max()
+                
+                dat+= [[ac.storename, 'Left', i, phase_duration, Alt_max, Mach_max, total_consumption, C1]]
+                dat+= [[ac.storename, 'Right', i, phase_duration, Alt_max, Mach_max, total_consumption, C2]]
+
+    dataframe = pd.DataFrame(dat,columns = ['Aircraft', 'Engine', 'Flight', 'Phase duration', 
+                                            'Alt_max', 'Mach_max', 'Total consumption', 'Consumption volume'])
+    
+    return dataframe
