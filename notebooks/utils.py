5190aa11aea07b1d4d3a35b0388f7cb413f17e68
DINH Quang Dung (Đinh Quang Dũng) <dqdung999@gmail.com>
2024-03-05 21:23:12 +0100

    Update utils.py

--- notebooks/utils.py
+++ notebooks/utils.py
@@ -142,46 +142,6 @@ def get_consumption(ac, phase: str = None, threshold: int = 0.05):
     
     return dataframe
 
-
-
-def extend(position, array, look, threshold):
-    # This function extend the from the input position to the left and right
-    # to find the cruise phase
-    left_pos,right_pos = position, position
-    # Search left position
-    # l = 0
-    while True:
-        avg_left = np.mean(array[left_pos - look:left_pos])
-        relative_dif = np.abs(avg_left - array[left_pos]/array[left_pos])
-        if relative_dif < threshold:
-            left_pos -= look #translate to the left
-        else: #check the next 3 interval to see the trend
-            next_five_mean = [np.mean(array[left_pos - (i+1)*look : left_pos - i*look]) for i in range(3)] 
-            next_rel_dif = [np.abs(next_five_mean[i] - array[left_pos - i*look]/array[left_pos - i*look]) for i in range(3)]
-            if np.mean(next_rel_dif) < threshold:
-                left_pos -= look//10
-            else:
-                # print(l)
-                break
-        # l+=1
-    # r = 0
-    # Search right position
-    while True:
-        avg_right = np.mean(array[right_pos : right_pos+look])
-        relative_dif = np.abs(avg_right - array[right_pos]/array[right_pos])
-        if relative_dif < threshold:
-            right_pos += look #translate to the right
-        else: #check the next 3 interval to see the trend
-            next_five_mean = [np.mean(array[right_pos + i*look : right_pos + (i+1)*look]) for i in range(3)] 
-            next_rel_dif = [np.abs(next_five_mean[i] - array[right_pos + i*look])/array[right_pos + i*look] for i in range(3)]
-            if np.mean(next_rel_dif) < threshold:
-                right_pos += look//10
-            else:
-                # print(r)
-                break
-        # r+=1
-    return left_pos, right_pos
-
 class RelativeIqr:
     """
         Une classe qui calcule les scores relatifs ou
